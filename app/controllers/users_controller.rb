291d9177a45d9e04ceff6a0611eac75cf6e7134a
Osong Agberndifor <agberndifor7@gmail.com>
2020-06-23 12:16:00 +0100

    Add sessions controller to manage users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     @user.save 
+    session[:user_id] = @user.id
 
     redirect_to root_path
   end
@@ -32,6 +33,7 @@ class UsersController < ApplicationController
   def destroy
     
     @user.destroy 
+    session[:user_id] = nil
 
     redirect_to root_path
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+
+
+  def actual_user?
+   current_user == @user 
+  end
 end
