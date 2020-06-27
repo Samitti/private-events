c5f9e5bfff08d3cb2da039394e85c2bdee6ad5a5
Osong Agberndifor <agberndifor7@gmail.com>
2020-06-27 21:56:31 +0100

    Remove some view logic

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,13 @@ module EventsHelper
   def event_creator?
     current_user == @event.creator
   end
+
+  def register_link(event_id: @event.id, user_id: current_user.id)
+    if @current_attendee
+      content_tag(:strong, "You have registered for this event")
+    else
+      link_to "Register for Event", event_join_path(event_id: @event.id, user_id: current_user.id), class: 'btn btn-secondary'
+    end
+  end
+  
 end
