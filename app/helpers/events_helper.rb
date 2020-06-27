1906753d021d741ddc4b5872c9790c469e5673d7
Osong Agberndifor <agberndifor7@gmail.com>
2020-06-27 22:08:56 +0100

    Add link for all users

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,12 +3,11 @@ module EventsHelper
     current_user == @event.creator
   end
 
-  def register_link(event_id: @event.id, user_id: current_user.id)
+  def register_link(*)
     if @current_attendee
-      content_tag(:strong, "You have registered for this event")
+      content_tag(:strong, 'You have registered for this event')
     else
-      link_to "Register for Event", event_join_path(event_id: @event.id, user_id: current_user.id), class: 'btn btn-secondary'
+      link_to 'Register for Event', event_join_path(event_id: @event.id, user_id: current_user.id), class: 'btn btn-secondary' # rubocop:disable Layout/LineLength
     end
   end
-  
 end
