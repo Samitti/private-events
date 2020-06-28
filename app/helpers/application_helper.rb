1996a5c629e24969040c6c66ad080bfeeca997e9
Osong Agberndifor <agberndifor7@gmail.com>
2020-06-28 13:11:18 +0100

    Remove view logic fromm header partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def user_name(*)
+    if current_user
+      link_to current_user.name, user_path(current_user), class: 'nav-link btn'
+    end
+  end
+
+  def user_links(*)
+    if current_user
+      link_to 'Sign Out', session_path, method: :delete, class: 'nav-link'
+    else
+      link_to 'Sign Up', new_user_path, class: 'nav-link'
+    end
+  end
+
+  def user_links2(*)
+    if !current_user
+      link_to 'Sign In', new_session_path, class: 'nav-link'
+    end
+  end
+  
 end
