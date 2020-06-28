e3919c7eddbadb38af98d1224518a49b7e055e86
Osong Agberndifor <agberndifor7@gmail.com>
2020-06-28 13:27:24 +0100

    Remove more view logic from users show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,16 @@ module UsersHelper
   def actual_user?
     current_user == @user
   end
+
+  def current_user_links(user)
+    if actual_user?
+      link_to 'Edit User Account', edit_user_path(user), class: 'btn btn-light'
+    end
+  end
+
+  def current_user_links2(user)
+    if actual_user?
+      link_to 'Delete Account', user_path(user), method: :delete, class: 'btn btn-light ml-2'
+    end
+  end
 end
